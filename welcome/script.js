ec3d214b5c34b26435a9c1cabac30a43a3a3eb12
Filipbagen <filip.bagen@gmail.com>
2018-12-29 15:11:59 +0100

    Added javascript
    Your name will now display on the screen.

--- welcome/script.js
+++ welcome/script.js
@@ -1,4 +1,5 @@
-var test = "test";
-document.getElementById("myInput").value = test;
-
-console.log(hello)
+function outputname() {
+    var name = document.getElementById("inputname").value;
+    document.getElementById("container1").style.display = "none";
+    document.getElementById("demo").innerHTML = "Welcome " + name;
+}
