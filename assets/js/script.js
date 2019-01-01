c2b73dd46cf0cb621e356e169938abd96227dd21
Filip Malm-Bägén <Filip.bagen@gmail.com>
2019-01-01 16:55:08 +0100

    Final changes
    You can now press enter to go to the next page. The first letter of your name will be capitalized.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ function outputname() {
     if (name) {
         document.getElementById("demo").innerHTML = "Welcome " + name;
         document.querySelector('body').classList.toggle('welcome')
-    } else {}
+    } else { }
 }
 
 function takeMeBack() {
